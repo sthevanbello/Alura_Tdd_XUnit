2e470ba2b70f97c275f4c3d61d3fc2921ac971ec
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-09-25 18:10:38 -0300

    First commit

---
+++ Alura.LeilaoOnline.Core/Leilao.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Alura.LeilaoOnline.Core
+{
+    public class Leilao
+    {
+        private IList<Lance> _lances;
+        public IEnumerable<Lance> Lances => _lances;
+        public string Peca { get; }
+
+        public Leilao(string peca)
+        {
+            Peca = peca;
+            _lances = new List<Lance>();
+        }
+
+        public void RecebeLance(Interessada cliente, double valor)
+        {
+            _lances.Add(new Lance(cliente, valor));
+        }
+
+        public void IniciaPregao()
+        {
+
+        }
+
+        public void TerminaPregao()
+        {
+
+        }
+    }
+}
