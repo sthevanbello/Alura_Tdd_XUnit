4335c715aeb4f2e144da71836db0ab607425f613
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-09-30 15:57:05 -0300

    Implemented Simple Test

---
+++ Alura.LeilaoOnline.ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using Alura.LeilaoOnline.Core;
+using System;
+
+namespace Alura.LeilaoOnline.ConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            var leilao = new Leilao("Van Gogh");
+            var homer = new Interessada(nome: "Homer Simpson", leilao: leilao);
+            var marge = new Interessada("Marge Simpson", leilao);
+
+            leilao.RecebeLance(homer, 800);
+            leilao.RecebeLance(marge, 1200);
+            leilao.RecebeLance(marge, 900);
+            leilao.RecebeLance(homer, 1000);
+            leilao.RecebeLance(marge, 1100);
+            leilao.RecebeLance(marge, 900);
+
+            leilao.TerminaPregao();
+
+            Console.WriteLine(leilao.Ganhador.Cliente.Nome); 
+            Console.WriteLine(leilao.Ganhador.Valor); 
+
+
+            Console.ReadKey();
+        }
+    }
+}
