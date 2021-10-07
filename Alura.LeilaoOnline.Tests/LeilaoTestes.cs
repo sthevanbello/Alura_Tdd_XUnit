d3acc915e4c7a1fd8260fc9df212f4872e188dc3
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-10-07 19:01:22 -0300

    Implemented theory and InlineData in method

--- Alura.LeilaoOnline.Tests/LeilaoTestes.cs
+++ Alura.LeilaoOnline.Tests/LeilaoTestes.cs
@@ -9,111 +9,38 @@ namespace Alura.LeilaoOnline.Tests
     public class LeilaoTestes
     {
 
-        [Fact]
-        public void LeilaoComTresClientes()
+        [Theory]
+        [InlineData(1200, new double[] { 800, 900, 1000, 1100, 1200 })]
+        [InlineData(1200, new double[] { 800, 1100, 1000, 1200, 1150 })]
+        [InlineData(800, new double[] { 800 })]
+        public void LeilaoComVariosClientes(double valorEsperado, double[] ofertas)
         {
             // Arrange - Cenário de entrada
             // Dado leilão com três clientes e lances realizados por eles
             var leilao = new Leilao("Van Gogh");
             var homer = new Interessada(nome: "Homer Simpson", leilao: leilao);
-            var marge = new Interessada("Marge Simpson", leilao);
-            var lisa = new Interessada("Lisa Simpson", leilao);
 
-            leilao.RecebeLance(homer, 800);
-            leilao.RecebeLance(marge, 900);
-            leilao.RecebeLance(lisa, 950);
-            leilao.RecebeLance(homer, 1000);
-            leilao.RecebeLance(marge, 1100);
-            leilao.RecebeLance(lisa, 1200);
 
+            foreach (var valor in ofertas)
+            {
+                leilao.RecebeLance(homer, valor);
+            }
 
             // Act - Método sob teste
             // Quando o pregão/leilão termina 
             leilao.TerminaPregao();
 
             Console.WriteLine("Leilão com vários lances");
-            
+
             // Assert
             // Então o valor esperado é o maior valor ofertado pelo cliente
             // O cliente ganhador é o que ofertou o maior lance
-            var valorEsperado = 1200;
-            var valorObtido = leilao.Ganhador.Valor;
 
-            Assert.Equal(valorEsperado, valorObtido);
-            Assert.Equal(lisa, leilao.Ganhador.Cliente);
-        }
-
-
-        [Fact]
-        public void LeilaoLancesOrdenadosPorValor()
-        {
-            // Arrange - Cenário de entrada
-            var leilao = new Leilao("Van Gogh");
-            var homer = new Interessada(nome: "Homer Simpson", leilao: leilao);
-            var marge = new Interessada("Marge Simpson", leilao);
-
-            leilao.RecebeLance(homer, 800);
-            leilao.RecebeLance(marge, 900);
-            leilao.RecebeLance(marge, 950);
-            leilao.RecebeLance(homer, 1000);
-            leilao.RecebeLance(marge, 1100);
-            leilao.RecebeLance(marge, 1200);
-
-            // Act - Método sob teste
-            leilao.TerminaPregao();
-
-            Console.WriteLine("Leilão com vários lances");
-            // Assert
-            var valorEsperado = 1200;
             var valorObtido = leilao.Ganhador.Valor;
 
             Assert.Equal(valorEsperado, valorObtido);
         }
 
-        [Fact]
-        public void LeilaoComApenasUmLance()
-        {
-            var leilao = new Leilao("Van Gogh");
-            var homer = new Interessada(nome: "Homer Simpson", leilao: leilao);
-            leilao.RecebeLance(homer, 800);
-            leilao.TerminaPregao();
-
-            var valorEsperado = 800;
-            var valorObtido = leilao.Ganhador.Valor;
-
-            Console.WriteLine("Leilão com apenas um lance");
-
-            Assert.Equal(valorEsperado, valorObtido);
-        }
-
-
-        [Fact]
-        public void LeilaoComVariosLances()
-        {
-            // Arrange - Cenário de entrada
-            var leilao = new Leilao("Van Gogh");
-            var homer = new Interessada(nome: "Homer Simpson", leilao: leilao);
-            var marge = new Interessada("Marge Simpson", leilao);
-
-            leilao.RecebeLance(homer, 800);
-            leilao.RecebeLance(marge, 1200);
-            leilao.RecebeLance(marge, 900);
-            leilao.RecebeLance(homer, 1000);
-            leilao.RecebeLance(marge, 1100);
-            leilao.RecebeLance(marge, 900);
-
-            // Act - Método sob teste
-            leilao.TerminaPregao();
-
-            Console.WriteLine("Leilão com vários lances");
-            // Assert
-            var valorEsperado = 1200;
-            var valorObtido = leilao.Ganhador.Valor;
-
-            Assert.Equal(valorEsperado, valorObtido);
-
-            
-        }
 
         [Fact]
         public void LeilaoSemLances()
